ddcd8c0ec10fa11e78c23ff63739ac181c40f13e
leeys1995 <60807751+leeys1995@users.noreply.github.com>
2021-10-26 19:45:05 +0900

    1.0 Initiallization

--- src/components/NweetFactory.js
+++ src/components/NweetFactory.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { dbService, storageService } from "fbase";
-import { v4 as uuidv4 } from "uuid";
+//import { v4 as uuidv4 } from "uuid";
 
 const NweetFactory = ( {userObj}) => {
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,10 @@
-import userEvent from "@testing-library/user-event";
+
 import { dbService } from "fbase";
-import { userEffect, useState } from "react";
+import {  useEffect, useState } from "react";
 import Nweet from "components/Nweet";
 import NweetFactory from "components/NweetFactory";
-import { useEffect } from "react/cjs/react.development";
-import { storage } from "firebase";
+
+//import { storage } from "firebase";
 
 const Home = ({userObj}) => {
 
